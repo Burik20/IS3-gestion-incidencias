f5b51663fd206cd5f7755d11ae98c05a41112792
Burik20 <50221852+Burik20@users.noreply.github.com>
2019-06-19 20:30:48 -0300

    closes #1. script clientes

---
+++ clientes.sql
@@ -0,0 +1,10 @@
+﻿--script para generar una tabla de clientes--
+create table clientes 
+	(
+dni double precision not null,
+	apellido character varying (30), 
+	
+ciudad character varying (30),
+
+	constraint dni primary key (dni)
+);
